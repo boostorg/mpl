11b9928353769a662f0d419e0e73425241fdb2bc
Neil Groves <neil@grovescomputing.com>
2010-05-11 22:20:19 +0000

    Improve compatibility with compilers other than GCC.
    [SVN r61922]

--- include/boost/mpl/eval_if.hpp
+++ include/boost/mpl/eval_if.hpp
@@ -4,8 +4,8 @@
 
 // Copyright Aleksey Gurtovoy 2000-2004
 //
-// Distributed under the Boost Software License, Version 1.0. 
-// (See accompanying file LICENSE_1_0.txt or copy at 
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 // See http://www.boost.org/libs/mpl for documentation.
@@ -43,7 +43,7 @@ struct eval_if
     BOOST_MPL_AUX_LAMBDA_SUPPORT(3,eval_if,(C,F1,F2))
 };
 
-// (almost) copy & paste in order to save one more 
+// (almost) copy & paste in order to save one more
 // recursively nested template instantiation to user
 template<
       bool C
