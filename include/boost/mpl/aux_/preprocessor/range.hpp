6fd793c787fa74c23306df208ef9de539040c29d
Bruno Dutra <brunocodutra@gmail.com>
2015-02-08 19:14:55 -0200

    keeping it simple

--- include/boost/mpl/aux_/preprocessor/range.hpp
+++ include/boost/mpl/aux_/preprocessor/range.hpp
@@ -15,21 +15,14 @@
 // $Revision$
 
 #include <boost/preprocessor/seq/subseq.hpp>
-#include <boost/preprocessor/tuple/to_seq.hpp>
+#include <boost/preprocessor/repetition/repeat.hpp>
 #include <boost/preprocessor/arithmetic/add.hpp>
-#include <boost/preprocessor/punctuation.hpp>
-#include <boost/preprocessor/enum_params.hpp>
-#include <boost/preprocessor/empty.hpp>
 
-#undef BOOST_MPL_PP_RANGE
+#define BOOST_MPL_PP_RANGE_ITEM(z,n,_) (n)
+
 #define BOOST_MPL_PP_RANGE(first, length) \
     BOOST_PP_SEQ_SUBSEQ( \
-        BOOST_PP_TUPLE_TO_SEQ( \
-                BOOST_PP_ADD(first, length), \
-                BOOST_PP_LPAREN() \
-                BOOST_PP_ENUM_PARAMS(BOOST_PP_ADD(first, length), BOOST_PP_EMPTY()) \
-                BOOST_PP_RPAREN() \
-        ), \
+        BOOST_PP_REPEAT(BOOST_PP_ADD(first,length), BOOST_MPL_PP_RANGE_ITEM, _), \
         first, length \
     ) \
 /**/
