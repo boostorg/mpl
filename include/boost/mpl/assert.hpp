4634ef904e4fc9b720f50a3012fa3adafdc095db
Anis Ladram <aladram@nvidia.com>
2022-03-10 15:20:45 -0500

    Fix -Wparentheses warning not ignored on NVCC
    BOOST_GCC is not defined for NVCC, therefore we should use BOOST_GCC_VERSION.

--- include/boost/mpl/assert.hpp
+++ include/boost/mpl/assert.hpp
@@ -184,7 +184,7 @@ template< typename P > struct assert_arg_pred_not
     typedef typename assert_arg_pred_impl<p>::type type;
 };
 
-#if defined(BOOST_GCC) && BOOST_GCC >= 80000
+#if defined(BOOST_GCC_VERSION) && BOOST_GCC_VERSION >= 80000
 #define BOOST_MPL_IGNORE_PARENTHESES_WARNING
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wparentheses"
